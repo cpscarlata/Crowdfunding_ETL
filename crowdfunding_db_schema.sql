e14b1a78319e95f0f5720a78dd682053cc2f9a91
Fawziyya Muhammad <66220123+fzyya@users.noreply.github.com>
2024-03-05 20:26:04 -0500

    Add files via upload

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE category (
+    category_id varchar   NOT NULL PRIMARY KEY,
+    category varchar   NOT NULL
+);
+
+CREATE TABLE contacts (
+    contact_id int   NOT NULL PRIMARY KEY,
+    first_name varchar   NOT NULL,
+    last_name varchar   NOT NULL,
+    email varchar   NOT NULL
+);
+
+CREATE TABLE subcategory (
+    subcategory_id varchar   NOT NULL PRIMARY KEY,
+    subcategory varchar   NOT NULL
+);
+
+CREATE TABLE campaign (
+    cf_id int   NOT NULL PRIMARY KEY,
+    contact_id int   NOT NULL,
+    company_name varchar   NOT NULL,
+    description varchar   NOT NULL,
+    goal float   NOT NULL,
+    pledged float   NOT NULL,
+    outcome varchar   NOT NULL,
+    backers_count int   NOT NULL,
+    country varchar   NOT NULL,
+    currency varchar   NOT NULL,
+    launched_date date   NOT NULL,
+    end_date date   NOT NULL,
+    category_id varchar   NOT NULL,
+    subcategory_id varchar   NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id),
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id)
+);
+
+SELECT * FROM campaign;
+SELECT * FROM category;
+SELECT * FROM contacts;
+SELECT * FROM subcategory;
